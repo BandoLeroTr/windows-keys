6e9f7b203e71fc243c5d52599cae40c5f085811f
BandoLeroTr <90865536+BandoLeroTr@users.noreply.github.com>
2023-01-14 18:15:30 +0300

    Update crack.py

--- crack.py
+++ crack.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkterminal import Terminal
-import os
 
 root = Tk()
 
@@ -10,10 +9,21 @@ list = [
     "Windows vista",
     "Windows 7",
     "Windows 8",
-    "Windows 8.1",
+    "Windows 8.1 Single Language",
     "Windows 10",
     "Windows 11"
 
+]
+
+list2 = [
+
+    "",
+    "",
+    "",
+    "",
+    "BB6NG-PQ82V-VRDPW-8XVD2-V8P66",
+    "",
+    ""
 ]
 listbox = Listbox(root, width=100)
 
@@ -22,30 +32,18 @@ for i in list:
 
 listbox.grid(row=0, column=0)
 
-terminal = Terminal(bg="black", fg="green", width=100)
+terminal = Terminal(bg="black", fg="green", font="font-source-sans-pro", width=100)
+terminal.shell = True
 terminal.grid(row=1, column=0)
 
 def select_item():
     for i in listbox.curselection():
 
-        if i == 0:
-            pass
+        terminal.run_command("slmgr.vbs /ipk {}".format(list2[i]))
+        terminal.run_command("slmgr.vbs /skms kms.lotro.cc")
+        terminal.run_command("slmgr.vbs /ato")
 
-        if i == 1:
-            pass
-
-        if i == 2:
-            pass
-
-        if i == 3:
-            pass
-
-        if i == 4:
-            os.system("slmgr.vbs /ipk BB6NG-PQ82V-VRDPW-8XVD2-V8P66")
-            os.system("slmgr.vbs /skms kms.lotro.cc")
-            os.system("slmgr.vbs /ato")
-
-button = Button(text="select", command = select_item)
+button = Button(text="select", command=select_item)
 button.grid(row=2, column=0)
 
 root.mainloop()
